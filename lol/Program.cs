80d40114d826a21b23651410316542cb2aeaf7b9
ayariii <90261694+ayariii@users.noreply.github.com>
2022-09-01 12:21:46 +0200

    Add files via upload

---
+++ lol/Program.cs
@@ -0,0 +1,12 @@
+﻿class Bil
+{
+  private string color = "Red";
+  private string type = "Audi R8";
+  static void Main(string[] args)
+  {
+    Bil minBil1 = new Bil();
+    Bil minBil2 = new Bil();
+    Console.WriteLine(minBil1.color);
+    Console.WriteLine(minBil2.type);
+  }
+}
